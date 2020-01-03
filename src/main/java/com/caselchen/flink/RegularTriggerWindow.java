da795845a9de163e0ef5928d68070b08cb3cf13b
chenshuai1 <chenshuai1@niwodai.net>
2020-01-03 14:18:12 +0800

    add clean logic

--- src/main/java/com/caselchen/flink/RegularTriggerWindow.java
+++ src/main/java/com/caselchen/flink/RegularTriggerWindow.java
@@ -33,12 +33,17 @@ public class RegularTriggerWindow {
     }
 
     // 采用KeyedProcessFunction处理每5分钟统计当天的平均值
+    // onTimer原来是处理early trigger逻辑，如果加上状态清理就变成了先判断currentKey所对应的状态有没有过期，
+    // 如果过期的话就直接从state中删除，如果没有过期就照常处理early trigger逻辑
     static class MyProcessFunction extends KeyedProcessFunction<Long, Long, Double> {
-        MapState<Long, Tuple2<Long, Long>> state;
+        private MapState<Long, Tuple2<Long, Long>> state;
+        private MapState<Long, Boolean> ttlState;
+        private final long TTL = Time.days(2).toMilliseconds();
 
         public void open(Configuration parameters) throws Exception {
             super.open(parameters);
             state = getRuntimeContext().getMapState(new MapStateDescriptor("dailyAvgData", Types.LONG, Types.TUPLE(Types.LONG, Types.LONG)));
+            ttlState = getRuntimeContext().getMapState(new MapStateDescriptor("ttl", Types.LONG, Types.BOOLEAN));
         }
 
         public void processElement(Long value, Context ctx, Collector<Double> out) throws Exception {
@@ -55,16 +60,29 @@ public class RegularTriggerWindow {
             avgData.f0 = avgData.f0 + 1;
             avgData.f1 = avgData.f1 + value;
             state.put(windowStart, avgData);
+
+            if (ttlState.get(windowStart) == null) {
+                long ttlTimer = windowStart + TTL;
+                ctx.timerService().registerProcessingTimeTimer(ttlTimer);
+                ttlState.put(windowStart, true);
+            }
         }
 
         public void onTimer(long timestamp, OnTimerContext ctx, Collector<Double> out) throws Exception {
             super.onTimer(timestamp, ctx, out);
-            Long key = ctx.getCurrentKey();
-            // 根据State计算结果
-            Tuple2<Long, Long> avgData = state.get(key);
-            Double result = avgData.f1 * 1.0d / avgData.f0;
-            System.out.println("windowStart = " + key);
-            out.collect(result);
+            Long windowStart = ctx.getCurrentKey();
+            Long now = System.currentTimeMillis();
+            if (now - windowStart >= TTL) {
+                // 清理状态
+                state.remove(windowStart);
+                ttlState.remove(windowStart);
+            } else {
+                // 根据State计算结果
+                Tuple2<Long, Long> avgData = state.get(windowStart);
+                Double result = avgData.f1 * 1.0d / avgData.f0;
+                System.out.println("windowStart = " + windowStart);
+                out.collect(result);
+            }
         }
 
     }
